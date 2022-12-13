12e2e21c6d8c84355b477daf5faf36e8c9c688b4
SaiaIlham <ilhamindra09@mhs.mdp.ac.id>
2022-12-13 09:01:11 +0700

    ERROR PARALEL

---
+++ serial.sum.py
@@ -0,0 +1,13 @@
+from mimetypes import suffix_map
+import os,re
+import time
+start = time.time()
+
+jumlah = 0
+for suffix in range (1,5000):
+    jumlah = jumlah + suffix
+    print("Jumlah ke-", suffix, "hasil",jumlah)
+
+
+end = time.time()
+print(end-start)
