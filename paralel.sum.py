c592a54e6262fd80a781b5e619740b20cb6692e3
SaiaIlham <ilhamindra09@mhs.mdp.ac.id>
2022-12-13 09:15:44 +0700

    STKP_SELASA
    masih error di paralel

--- paralel.sum.py
+++ paralel.sum.py
@@ -1,19 +1,23 @@
 import os,re, threading
 import time
 
-class jumlah(threading. Thread):
-    def__init__(self, awal, akhir):
+class jumlah(threading.Thread):
+    def__init__(self,awal,akhir):
         threading.Thread.__init__(self)
         self.awal=awal 
         self.akhir=akhir
-    def run (self):
+    def run(self):
         self.hasil=0
         for suffix in range (self.awal, self.akhir+1):
             self.hasil = self.hasil = suffix
+            print(self.suffix, "=", self_hasil)
     def hasil(self):
         return self.hasil
 
 start = time.time()
 
+hitung1=jumlah(1, 5000000)
+print(hitung1.awal)
+print(hitung1.akhir)
 end = time.time()
 print(end-start)
