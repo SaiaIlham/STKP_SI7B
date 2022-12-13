e683876e3aefe58f1d2e7769a66cba1fa014d737
SaiaIlham <ilhamindra09@mhs.mdp.ac.id>
2022-12-13 09:21:22 +0700

    Update Paralel
    sudah aman

--- paralel.sum.py
+++ paralel.sum.py
@@ -2,22 +2,32 @@ import os,re, threading
 import time
 
 class jumlah(threading.Thread):
-    def__init__(self,awal,akhir):
+    def __init__(self, awal, akhir):
         threading.Thread.__init__(self)
-        self.awal=awal 
+        self.awal=awal
         self.akhir=akhir
+
     def run(self):
         self.hasil=0
         for suffix in range (self.awal, self.akhir+1):
-            self.hasil = self.hasil = suffix
-            print(self.suffix, "=", self_hasil)
+            self.hasil = self.hasil + self.suffix
+            print(self.suffix, "=", self.hasil)
+
     def hasil(self):
         return self.hasil
 
 start = time.time()
 
-hitung1=jumlah(1, 5000000)
-print(hitung1.awal)
-print(hitung1.akhir)
+#memanggil thread
+hitung1=jumlah(1, 5000)
+hitung2=jumlah(50001, 10000)
+
+#menjalankan thread
+hitung1.start()
+hitung2.hasil()
+
+#menampilkan hasil thread
+hitung1.hasil()
+
 end = time.time()
 print(end-start)
